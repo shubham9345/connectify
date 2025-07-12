81d27ed4202e9e2112e525e6f378661ccc74a612
shubham <kumarshubham2005a@gmail.com>
2025-07-12 22:58:48 +0530

    SecurityConfig updated

--- Connectify/src/main/java/com/connectify/demo/config/SecurityConfig.java
+++ Connectify/src/main/java/com/connectify/demo/config/SecurityConfig.java
@@ -113,7 +113,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8000", "http://127.0.0.1:5500", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8000", "http://127.0.0.1:5500", "http://localhost:5173","https://connectify-ten-xi.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
